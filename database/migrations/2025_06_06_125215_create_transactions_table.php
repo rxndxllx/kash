3938beb37b64c3955c891962908a0373f6962327
Randalle <randalle.mpm@gmail.com>
2025-06-06 21:12:49 +0800

    Create migrations for users, accounts, transactions

---
+++ database/migrations/2025_06_06_125215_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("transactions", function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id")->constrained("users");
+            $table->foreignId("account_id")->constrained("accounts");
+            $table->foreignId("category_id")->constrained("categories");
+            $table->double("amount");
+            $table->string("type");
+            $table->timestamp("transacted_at");
+            $table->string("note");
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists("transactions");
+    }
+};
---
+++ database/migrations/2025_06_06_125248_create_transaction_tags_table.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("transaction_tags", function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("transaction_id")->nullable()->constrained("transactions");
+            $table->foreignId("tag_id")->nullable()->constrained("tags");
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists("transaction_tags");
+    }
+};
